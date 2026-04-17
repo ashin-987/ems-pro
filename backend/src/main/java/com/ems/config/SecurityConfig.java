89131be52042c0fdca7b9ca390040de35dc6367e
ASHIN Port <ashinsh45@gmail.com>
2026-04-17 10:21:08 +0530

    fix: cors allow vercel origin

--- backend/src/main/java/com/ems/config/SecurityConfig.java
+++ backend/src/main/java/com/ems/config/SecurityConfig.java
@@ -79,8 +79,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "ems-pro-rvdv.vercel.app"
-                                        
+                "https://ems-pro-rvdv.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
